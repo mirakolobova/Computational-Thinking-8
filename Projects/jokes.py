4fb585b08101f3c8c4f08b7fada5b251eda8b558
mirakolobova <mirakolobova@seattleacademy.org>
2025-12-05 16:38:16 +0000

    Test

---
+++ Projects/jokes.py
@@ -0,0 +1,3 @@
+print("How do you organize a space-themed party?")
+input("")
+print("You planet")
--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("My favorite color is orange")    
+print("I have a dog")           
+print("My brother is 8")
