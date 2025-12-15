d85cdc80f68a4a07c42e0f3d7bae2f866ace04d2
mirakolobova <mirakolobova@seattleacademy.org>
2025-12-15 22:25:10 +0000

    ABCDEFG

--- Projects/Tester.py
+++ Projects/Tester.py
@@ -1,3 +1,10 @@
 print("Hello!")
 name = input("What is your name?")
 print(f"Hello {name}")
+
+answer=input("How do you like your steak cooked?")
+if answer == "medium":
+    print("I like medium too! :D")
+else:
+    print("Cool :)")
+
---
+++ Projects/project2.py
@@ -0,0 +1,54 @@
+dog_points = 0
+cat_points = 0
+fish_points = 0
+answer1 = input("Do you prefer A; Big animals B; Medium animals C; Small animals    ")
+if answer1 == "A":
+    dog_points += 1
+elif answer1 == "B":
+    cat_points += 1
+elif answer1 == "C":
+    fish_points += 1
+
+answer2 = input("Do you prefer A; Fluffy B; Not fluffy      ")
+if answer2 == "A":
+    dog_points += 1
+    cat_points += 1
+elif answer2 == "B":
+    fish_points += 1
+
+answer3 = input("What can you handle? A High maintenance B Low maintenance      ")
+if answer3 == "A":
+    dog_points += 1
+    cat_points += 1
+elif answer3 == "B":
+    fish_points += 1
+
+answer4 = input("Can this pet be loud? A; Yes B; No        ")
+if answer4 == "A" or answer4 == "D":
+    dog_points += 1
+elif answer3 == "B":
+    cat_points += 1
+    fish_points += 1
+
+answer5 = input("What is your favorite animal out of these? A; Dog B; Cat C; Fish       ")
+if answer5 == "A":
+    dog_points += 1
+    cat_points -= 1
+    fish_points -= 1
+elif answer5 == "B":
+    cat_points += 1
+    fish_points -= 1
+    dog_points -= 1
+elif answer5 == "C":
+    fish_points += 1
+    cat_points -= 1
+    dog_points -= 1
+
+if dog_points >= cat_points and dog_points >= fish_points:
+    print("You are a dog person")
+elif fish_points >= cat_points and fish_points >= dog_points:
+    print("You are a fish person")
+elif cat_points >= dog_points and cat_points >= fish_points:
+    print("You are a dog person")
+else:
+    print("You are tied")
